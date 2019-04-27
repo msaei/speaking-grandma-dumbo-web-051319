3582859d06e85b8273ed86b8ec1f8e6e0e7d80eb
beta-scanner-6136 <mahdi.saeikhalili@gmail.com>
2019-04-27 03:51:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma(speach)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-
+  if speach.Upcase
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
